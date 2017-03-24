bdeafeb46b037b383deb163461b39cd2ebaea906
Igor Uvarov <kuflash@ya.ru>
2017-03-24 23:05:24 +0300

    feat: add selectors for tasks

---
+++ src/redux/modules/tasks/selectors.js
@@ -0,0 +1,34 @@
+import { createSelector, createSelectorCreator } from 'reselect';
+import {
+  pipe,
+  values,
+  omit,
+  sort,
+  prop,
+  ascend,
+  descend,
+  cond,
+  equals,
+  memoize,
+} from 'ramda';
+
+const createDeepEqualSelector = createSelectorCreator(memoize, equals);
+
+export const getTasksItems = state => state.tasks.items;
+export const getActiveTaskId = state => state.tasks.activeTaskId || '';
+export const getSortRuleByProperty = (
+  (state, { sortProperty, sortDirection }) => cond([
+    [equals('asc'), () => ascend(prop(sortProperty))],
+    [equals('desc'), () => descend(prop(sortProperty))],
+  ])(sortDirection)
+);
+
+export const getCompletedTasks = createSelector(
+  [getTasksItems, getActiveTaskId],
+  (tasks, activeTaskId) => pipe(omit(activeTaskId), values)(tasks),
+);
+
+export const getSortedCompletedTasks = createDeepEqualSelector(
+  [getCompletedTasks, getSortRuleByProperty],
+  (tasks, sortRule) => sort(sortRule, tasks),
+);

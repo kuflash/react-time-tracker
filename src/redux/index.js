8e6fc92230b72a329b2155dacb4c55449de47470
Igor Uvarov <kuflash@ya.ru>
2017-05-20 13:47:24 +0300

    feat: apply redux-thunk

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import persistState from 'redux-localstorage';
 import reducers from './modules';
 
-export default () => createStore(
+const createStoreWithMiddleware = applyMiddleware(
+  thunkMiddleware,
+)(createStore);
+
+export default () => createStoreWithMiddleware(
   reducers,
   persistState(null, { key: 'ku-react-time-tracker' }),
 );
